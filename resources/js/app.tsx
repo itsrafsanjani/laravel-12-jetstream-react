c1bacbe146c2edbdd228ebc4e95933916ec9fb0d
Al-Amin Firdows <alaminfirdows@gmail.com>
2025-06-23 21:59:49 +0600

    refactor: apply jetstream

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,6 @@
 import '../css/app.css';
 
+import { RouteContext } from '@/Hooks/useRoute';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
@@ -9,11 +10,15 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <RouteContext.Provider value={(window as any).route}>
+                <App {...props} />
+            </RouteContext.Provider>,
+        );
     },
     progress: {
         color: '#4B5563',
--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
+            '@': '/resources/js',
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
